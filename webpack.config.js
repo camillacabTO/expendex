69403a69c656cf087b0af4a469cfa4f2cbc0a54e
Camila B <camillacab@hotmail.com>
2021-03-08 19:02:10 -0500

    fixed env?

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack');
 
 module.exports = env => {
   let prodMode = env.production
@@ -26,7 +27,17 @@ module.exports = env => {
         }
       ]
     },
-    plugins: [],
+    plugins: [new webpack.DefinePlugin({
+       NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+       FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+       FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+       FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+       FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+       FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+       FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+       FIREBASE_APP_ID: JSON.stringify(process.env.FIREBASE_APP_ID),
+       FIREBASE_MEASUREMENT_ID: JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
+    })],
     devtool: prodMode ? false : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
